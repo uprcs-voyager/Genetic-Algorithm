578da0dfee6f103792f51865c3685c9a6d2a4d07
radeet <greenglass0k@gmail.com>
2025-10-05 23:13:06 +0800

    parsing data complete

--- main.py
+++ main.py
@@ -14,16 +14,36 @@ with open('dataset/Jadwal Kuliah IF ITK - Detail.csv', mode= 'r') as file :
             parts = specific_string   
             days = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat"]
             for day in days :
-                string_index = specific_string.find(day)
-                if string_index != -1 :
-                        print(f"Hari: {day} Berada pada index: {string_index}")
+                day_index = specific_string.find(day)
+                if day_index != -1 :
                         get_lines = parts
-                        get_lines = get_lines[:string_index]
-                        dosen_kotor = get_lines
-                        print(f"Dosen: {get_lines}\n")
+                        get_lines = get_lines[:day_index]
+                        dosen_unclean = get_lines
+                        dosen_cleaned = dosen_unclean.strip()
+                        
+                        ruangan_index = specific_string.find("Ruang:")
+                        start_index = ruangan_index
+                        kapasitas_index = specific_string.find(", Kapasitas")
+                        ruangan = specific_string[start_index:kapasitas_index]
+                        replace_room_uncleaned = ruangan.replace("Ruang: ", "")
+                        replace_room_cleaned = replace_room_uncleaned.strip()
+
+                        ruangan_index = specific_string.find("Ruang:")
+                        start_index = day_index + len(day)
+                        end_index = ruangan_index
+                        sesi = specific_string[start_index:end_index]
+                        sesi_uncleaned = sesi.replace(",","")
+                        sesi_cleaned = sesi_uncleaned.strip()
+                      
+                        print(f"Dosen: {dosen_cleaned}")
+                        print(f"Hari: {day} || index {day_index} ||")
+                        print(f"Sesi: {sesi_cleaned}")
+                        print(f"Ruangan: {replace_room_cleaned}\n")
+
+
+
                         break;
-                
-            print(parts)
+        print(parts) 
      
         print(lines)
         print('\n')
