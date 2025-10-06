50ccdedba24ce8c9c06c32f2fc193938b1e40af9
radeet <greenglass0k@gmail.com>
2025-10-06 12:45:38 +0800

    pre-processed data completed

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import csv
-import pandas
 
 
 # IMPORTING DATA USING CSV
 print("Data read using csv DictReader")
 print("\n")
+
+data_jadwal_clean = []
+
+
+
+
 with open('dataset/Jadwal Kuliah IF ITK - Detail.csv', mode= 'r') as file :
     
     csvFile = csv.DictReader(file)
@@ -40,17 +45,71 @@ with open('dataset/Jadwal Kuliah IF ITK - Detail.csv', mode= 'r') as file :
                         print(f"Sesi: {sesi_cleaned}")
                         print(f"Ruangan: {replace_room_cleaned}\n")
 
-
+                        # Dictionary
+                        dictionary = {
+                             'Kode_MK': '',
+                             'Mata_Kuliah': '',
+                             'Kelas': '',
+                             'Dosen': '',
+                             'Hari': '',
+                             'Sesi': '',
+                             'Ruangan': '',
+                        }
+                        dictionary['Kode_MK'] = lines['Kode MK']
+                        dictionary['Mata_Kuliah'] = lines['Mata Kuliah']
+                        dictionary['Kelas'] = lines['Kelas']
+                        dictionary['Dosen'] = dosen_cleaned
+                        dictionary['Hari'] = day
+                        dictionary['Sesi'] = sesi_cleaned
+                        dictionary['Ruangan'] = replace_room_cleaned
+                        print(f"This is the dictionary: {dictionary}")
+                        
+                        data_jadwal_clean.append(dictionary)
 
                         break;
         print(parts) 
-     
+    
+
         print(lines)
         print('\n')
+    print(data_jadwal_clean)
+    print('\n')
+
+daftar_kelas = []
+kelas_yang_sudah_dicatat = set()
+
+semua_dosen = set()
+semua_ruangan = set()
+semua_sesi = set()
+
+for data in data_jadwal_clean :
+        
+    identifier = (data['Kode_MK'], data['Mata_Kuliah'], data['Kelas'])
+    
+    if identifier not in kelas_yang_sudah_dicatat :
+            kelas_yang_sudah_dicatat.add(identifier)
+
+            dictionary_new = {
+                'Kode_MK': data['Kode_MK'],
+                'Mata_Kuliah': data['Mata_Kuliah'], 
+                'Kelas': data['Kelas']
+                }    
+            daftar_kelas.append(dictionary_new)
+    semua_dosen.add(data['Dosen'])
+    semua_ruangan.add(data['Ruangan'])
+    semua_sesi.add(data['Sesi'])
+
+            
+print(f"Ini dictionary yepyep: {daftar_kelas}")
+        
+
+# debugging
+print(f"Jumlah jadwal yang berhasil di-parse: {len(data_jadwal_clean)}")
+print(f"Jumlah kelas unik yang harus dijadwalkan: {len(daftar_kelas)}")
+print(f"Jumlah dosen unik: {len(semua_dosen)}")
+print(f"Jumlah ruangan unik: {len(semua_ruangan)}")
+print(f"Jumlah sesi unik: {len(semua_sesi)}")
+
+
 
 print("\n\n\n")
-# # IMPORTING DATA USING PANDAS
-# print("Data read using pandas")
-# print("\n")
-# excelPanda = pandas.read_excel('dataset/Jadwal Kuliah IF ITK.xlsx')
-# print(excelPanda)
